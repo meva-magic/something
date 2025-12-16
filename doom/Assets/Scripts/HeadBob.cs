9650b3b7f09929f8d75308eaa4ee27c83b8676f8
saharov-dev <saharov.dev@gmail.com>
2025-12-16 21:59:56 +0300

    head bob

--- doom/Assets/Scripts/HeadBob.cs
+++ doom/Assets/Scripts/HeadBob.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class HeadBob : MonoBehaviour
 {
-    [Range(0.001f, 0.01f)]
-    [SerializeField] private float amount = 0.002f;
+    [Range(0.001f, 0.05f)]
+    [SerializeField] private float amount = 0.03f;
 
     [Range(10f, 100f)]
-    [SerializeField] private float smooth = 10f;
+    [SerializeField] private float smooth = 69f;
 
     [Range(1f, 30f)]
     [SerializeField] private float frequency = 10f;
 
     private float inputMagnitude;
     private Vector3 startPos;
-    private float bobTimer;
+    private float timer = 0f;
 
     private void Start()
     {
@@ -33,22 +33,22 @@ public class HeadBob : MonoBehaviour
 
         if (inputMagnitude > 0)
         {
-            bobTimer += Time.deltaTime * frequency;
-            Vector3 newPosition = startPos + CalculateHeadBob(bobTimer);
-            transform.localPosition = Vector3.Lerp(transform.localPosition, newPosition, smooth * Time.deltaTime);
+            timer += Time.deltaTime * frequency;
+            Vector3 targetPos = CalculateBobPosition(timer);
+            transform.localPosition = Vector3.Lerp(transform.localPosition, startPos + targetPos, smooth * Time.deltaTime);
         }
         else
         {
-            bobTimer = 0;
+            timer = 0f;
             transform.localPosition = Vector3.Lerp(transform.localPosition, startPos, smooth * Time.deltaTime);
         }
     }
 
-    private Vector3 CalculateHeadBob(float timer)
+    private Vector3 CalculateBobPosition(float timer)
     {
         Vector3 pos = Vector3.zero;
-        pos.y += Mathf.Sin(timer) * amount * 1.4f;
-        pos.x += Mathf.Cos(timer * 0.5f) * amount * 1.6f;
+        pos.y = Mathf.Sin(timer) * amount * 1.4f;
+        pos.x = Mathf.Cos(timer / 2f) * amount * 1.6f;
         return pos;
     }
 }
