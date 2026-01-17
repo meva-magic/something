c8addd09550d1a35b8847b74be1a8893d3c17e00
saharov-dev <saharov.dev@gmail.com>
2026-01-17 21:43:29 +0300

    add dialogue

---
+++ doom/Assets/Scripts/NPC/NPC.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class NPC : MonoBehaviour
+{
+    [SerializeField] private NPCScriptableObject dialogueData;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TMP_Text dialogueText, nameText;
+    [SerializeField] private Image portrait;
+
+    private int dialogueIndex;
+    private bool isTyping, isDialogueActive;
+
+    public bool CanInteract()
+    {
+        return !isDialogueActive;
+    }
+
+    public void Interact()
+    {
+        if(dialogueData == null || !isDialogueActive)
+        {
+            return;
+        }
+
+        if(isDialogueActive)
+        {
+            NextLine();
+        }
+
+        else
+        {
+            StartDialogue();
+        }
+    }
+
+
+    public void StartDialogue()
+    {
+        isDialogueActive = true;
+        dialogueIndex = 0;
+
+        nameText.SetText(dialogueData.name);
+        portrait.sprite = dialogueData.portrait;
+
+        dialoguePanel.SetActive(true);
+
+        StartCoroutine(TypeLine());
+    }
+
+
+    private void NextLine()
+    {
+        if(isTyping)
+        {
+            StopAllCoroutines();
+            dialogueText.SetText(dialogueData.dialogueLines[dialogueIndex]);
+            isTyping = false;
+        }
+
+        else if(++dialogueIndex < dialogueData.dialogueLines.Length)
+        {
+            StartCoroutine(TypeLine());
+        }
+
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+
+    private IEnumerator TypeLine()
+    {
+        isTyping = true;
+        dialogueText.SetText("");
+
+        foreach(char letter in dialogueData.dialogueLines[dialogueIndex])
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(dialogueData.typingSpeed);
+        }
+
+        isTyping = false;
+
+        if(dialogueData.skipLines.Length > dialogueIndex && dialogueData.skipLines[dialogueIndex])
+        {
+            yield return new WaitForSeconds(dialogueData.skipDelay);
+
+            NextLine();
+        }
+    }
+
+    
+    private void EndDialogue()
+    {
+        StopAllCoroutines();
+        isDialogueActive = false;
+        dialogueText.SetText("");
+        dialoguePanel.SetActive(false);
+    }
+}
---
+++ doom/Assets/Scripts/NPC/NPCScriptableObject.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="NewNPCDialogue", menuName = "NPC Dialogue")]
+
+public class NPCScriptableObject : ScriptableObject
+{
+    public string name;
+    public Sprite portrait;
+    public AudioClip voice;
+
+    [SerializeField] private float voicePitch = 1f;
+    
+    public string[] dialogueLines;
+    public bool[] skipLines;
+    public float skipDelay = 1.5f;
+
+    public float typingSpeed = 0.05f;    
+}
